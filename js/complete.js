39bbd2a44871d00c7bf8a8f5ee685185722ad5e7
toimurhasan <2imur.hasan@gmail.com>
2025-03-01 15:16:21 +0600

    task log feature added

--- js/complete.js
+++ js/complete.js
@@ -5,5 +5,20 @@ for (let i = 0; i < completeBtns.length; i++) {
     event.preventDefault();
     alert("Board updated successfully");
     this.setAttribute("disabled", true);
+
+    const taskRemaining = parseInt(document.getElementById("task-remaining").innerText);
+    const taskDone = parseInt(document.getElementById("task-done").innerText);
+
+    document.getElementById("task-remaining").innerText = taskRemaining - 1;
+    document.getElementById("task-done").innerText = taskDone + 1;
+
+    const grandParent = this.parentNode.parentNode;
+    const taskName = grandParent.querySelector("h3").innerText;
+
+    const taskLog = document.createElement("div");
+    taskLog.classList.add("bg-blue-100", "p-2", "rounded-lg", "text-gray-800");
+    taskLog.innerHTML = `You have Complete The Task ${taskName} at 12:48:15 PM`;
+
+    document.getElementById("task-logs").appendChild(taskLog);
   });
 }
